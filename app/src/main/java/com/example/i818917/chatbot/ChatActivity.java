962dc0048feee3c51dcac7b70790cb6299456378
Greg McClement <greg.mcclement@gmail.com>
2017-04-28 15:55:32 -0700

    Rejected commit: Default

---
+++ app/src/main/java/com/example/i818917/chatbot/ChatActivity.java
@@ -0,0 +1,13 @@
+package com.example.i818917.chatbot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
