a5b29a0afd03a17fd67ef053ce425d0f2b3ad2aa
Greg McClement <greg.mcclement@gmail.com>
2017-04-29 17:11:57 -0700

    Have the basic workflow without the neural net. Pressing start will cause input through voice. Then the input will be echo using a UK voice and more voice input will occur.

--- app/src/main/java/com/example/i818917/chatbot/ChatActivity.java
+++ app/src/main/java/com/example/i818917/chatbot/ChatActivity.java
@@ -1,8 +1,10 @@
+
 package com.example.i818917.chatbot;
 
 import android.content.Intent;
 import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,49 +15,69 @@ import java.util.Locale;
 
 public class ChatActivity extends AppCompatActivity {
 
-    TextToSpeech mtts;
+  private static final int SPEECH_REQUEST_CODE = 0;
+  TextToSpeech mTTS;
+  ToggleButton mTB;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_chat);
 
-        mtts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int status) {
-                mtts.setLanguage(Locale.UK);
-            }
-        });
+    mTTS = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+      @Override
+      public void onInit(int status) {
+        mTTS.setLanguage(Locale.UK);
+      }
+    });
+    final ChatActivity that = this;
+    mTTS.setOnUtteranceProgressListener(new UtteranceProgressListener() {
+      @Override
+      public void onStart(String utteranceId) {
 
-        ToggleButton tb = (ToggleButton) findViewById(R.id.toggleButton);
-        tb.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //mtts.speak("Hello World!", TextToSpeech.QUEUE_FLUSH, null);
-                displaySpeechRecognizer();
-            }
-        });
+      }
+
+      @Override
+      public void onDone(String utteranceId) {
+        that.displaySpeechRecognizer();
+      }
+
+      @Override
+      public void onError(String utteranceId) {
+
+      }
+    });
+
+    mTB = (ToggleButton) findViewById(R.id.toggleButton);
+    mTB.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        displaySpeechRecognizer();
+      }
+    });
+  }
+
+  private void displaySpeechRecognizer() {
+    if (!mTB.isChecked()){
+      return;
     }
+    Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+    // Start the activity, the intent will be populated with the speech text
+    startActivityForResult(intent, SPEECH_REQUEST_CODE);
+  }
 
-    private static final int SPEECH_REQUEST_CODE = 0;
-
-    private void displaySpeechRecognizer() {
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        // Start the activity, the intent will be populated with the speech text
-        startActivityForResult(intent, SPEECH_REQUEST_CODE);
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode,
+                                  Intent data) {
+    if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+      List<String> results = data.getStringArrayListExtra(
+          RecognizerIntent.EXTRA_RESULTS);
+      String spokenText = results.get(0);
+      mTTS.speak(spokenText,TextToSpeech.QUEUE_ADD, null, "message");
+      return;
     }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode,
-                                    Intent data) {
-        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
-            List<String> results = data.getStringArrayListExtra(
-                    RecognizerIntent.EXTRA_RESULTS);
-            String spokenText = results.get(0);
-            mtts.speak(spokenText, TextToSpeech.QUEUE_FLUSH, null);
-        }
-        super.onActivityResult(requestCode, resultCode, data);
-    }
+    super.onActivityResult(requestCode, resultCode, data);
+  }
 
 }
