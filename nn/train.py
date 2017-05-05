6c674d8a379f94a05d5a5b8ed6fba05292c5b751
Greg McClement <greg.mcclement@sap.com>
2017-05-05 23:24:39 +0000

    wip

--- nn/train.py
+++ nn/train.py
@@ -5,6 +5,7 @@ import numpy as np
 import math
 from collections import Counter
 import pdb
+from tf.contrib.legacy_seq2seq import embedding_rnn_seq2seq
 
 
 INPUT_FILE_NAME = 'input.txt'
@@ -51,13 +52,22 @@ load_utterances(OUTPUT_FILE_NAME, outputs, EOS)
 print("Outputs")
 print(outputs)
 
-BATCH_SIZE = 5
-MAX_LENGTH = 4
+batch_size = 5
+seq_length = 4
 embedding_size = 128
+cell_size = 96
+num_layers = 3
 
 # convert to embeddings
 
-embeddings = tf.Variable(tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
-
 nce_weights = tf.Variable(tf.truncated_normal([vocabulary_size, embedding_size], stddev=1.0 / math.sqrt(embedding_size)))
 nce_biases = tf.Variable(tf.zeros([vocabulary_size]))
+
+encoder_inputs = tf.placeholder(tf.int32, shape=[batch_size, seq_length])
+decoder_inputs = tf.placeholder(tf.int32, shape=[batch_size, seq_length])
+
+single_cell = tf.contrib.rnn.BasicLSTMCell(cell_size)
+cell = tf.contrib.rnn.MultiRNNCell([single_cell for _ in range(num_layers)])
+
+outputs, states = embedding_rnn_seq2seq(encoder_inputs, decoder_inputs, cell, vocabulary_size, vocabulary_size, embedding_size, output_projection=None, feed_previous=False)
+
